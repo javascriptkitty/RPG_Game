6b3c0a4acd37a0c552f2130663b22891a42066d3
Cristina Alekseeva <cristina.alekseeva@gmail.com>
2019-05-28 17:36:56 -0700

    added charachters and styles

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+var ji = {
+  healthPoints: 180,
+  attackPower: 10,
+  counterAttackPower: 18
+};
+
+var aysa = {
+  healthPoints: 150,
+  attackPower: 8,
+  counterAttackPower: 15
+};
+
+var chen = {
+  healthPoints: 200,
+  attackPower: 12,
+  counterAttackPower: 20
+};
+
+var taoshi = {
+  healthPoints: 120,
+  attackPower: 6,
+  counterAttackPower: 15
+};
